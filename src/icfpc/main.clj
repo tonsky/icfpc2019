7298ef5ceef631ccbe6b3d4e3202a4e2a00e9de9
Nikita Prokopov <niki@tonsky.me>
2019-06-21 16:58:56 +0300

    Main runner

--- src/icfpc/main.clj
+++ src/icfpc/main.clj
@@ -1,29 +1,20 @@
 (ns icfpc.main
   (:require
-   [icfpc.routes :as routes]
-   [ring.adapter.jetty :as jetty]
-   [com.stuartsierra.component :as component]))
-
-(defrecord Server [opts jetty]
-  component/Lifecycle
-  (start [this]
-    (when (nil? jetty)
-      (println "Starting web server at" (str (:host opts) ":" (:port opts)))
-      (assoc this :jetty
-        (jetty/run-jetty #'routes/routes opts))))
-  (stop [this]
-    (when (some? jetty)
-      (println "Stopping web server")
-      (.stop jetty)
-      (assoc this :jetty nil))))
-
-(defn server [opts]
-  (->Server (merge {:host "localhost" :port 8080} opts) nil))
-
-(defn system [opts]
-  (component/system-map
-    :server (server (:server opts))))
+   [icfpc.bot :as bot]
+   [icfpc.level :as level]
+   [clojure.string :as str]
+   [clojure.java.io :as io]))
 
 (defn -main [& args]
-  (-> (system {})
-    (component/start)))
+  (doseq [path (->> (file-seq (io/file "resources/part-1-initial"))
+                 (map #(.getPath %))
+                 (filter #(str/ends-with? % ".desc"))
+                 sort)]
+    (try
+      (let [[_ name] (re-matches #".*/(prob-\d\d\d)\.desc" path)
+            _        (println "Solving" name)
+            level    level/prob-001
+            sln      (bot/solve level false)]
+        (spit (str "resources/solutions/" name ".sol") sln))
+      (catch Exception e
+        (.printStackTrace e)))))
