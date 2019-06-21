765db428e3c111ec2e08de679bf6efab0b7fb6f7
Nikita Prokopov <niki@tonsky.me>
2019-06-21 18:57:08 +0300

    first 50 solutions

--- src/icfpc/main.clj
+++ src/icfpc/main.clj
@@ -13,18 +13,30 @@
     (println " found" sln)
     (spit (str "resources/solutions/" name ".sol") sln)))
 
-(defn -main [& args]
+(defn skip-till [n xs]
+  (cond
+    (nil? n) xs
+    (string? n) (drop (Integer/parseInt n) xs)
+    (number? n) (drop n xs)))
+
+(defn -main [& [skip]]
   (doseq [name (->> (file-seq (io/file "resources/part-1-initial"))
                  (map #(.getPath %))
                  (filter #(str/ends-with? % ".desc"))
                  (map #(second (re-matches #".*/(prob-\d\d\d)\.desc" %)))
-                 sort)]
+                 sort
+                 (skip-till skip))]
     (try
       (solve name)
       (catch Exception e
         (.printStackTrace e)))))
 
+(defn print-solve [name]
+  (bot/print-level (level/load-level (str name ".desc")))
+  (solve name))
+
 (comment
   (icfpc.main/solve "prob-002")
+  (icfpc.main/print-solve "prob-002")
   (icfpc.bot/solve (icfpc.level/load-level "prob-002.desc"))
 )
