f1b4f47c91a0374b96bc264062a92c14061022d0
Pavel Sergeev <pavel.sergeev@jetbrains.com>
2019-06-21 14:39:29 +0300

    parse text

---
+++ src/icfpc/parser.clj
@@ -0,0 +1,27 @@
+(ns icfpc.parser
+  (:require [clojure.string :refer [split]]))
+
+(defn parse-points [points-str]
+  (if (not-empty points-str)
+    (let [points-seq (split (->> points-str
+                                 (drop 1)
+                                 (drop-last 1)
+                                 (apply str))
+                            #"\),\(")]
+      (mapv (fn [p]
+              (mapv #(Integer/parseInt %) (split p #",")))
+            points-seq))
+    []))
+
+(defn parse-buster [buster-str]
+  (let [[type point-str] (split-at 1 buster-str)]
+    [type (first (parse-points point-str))]))
+
+(defn parse [task-name]
+  (let [description (slurp (str "resources/part-1-initial/" task-name))
+        [_ level start obstacles busters] (re-matches #"(.*)#(.*)#(.*)#(.*)" description)]
+    {:start (first (parse-points start))
+     :level (parse-points level)
+     :obstacles (mapv parse-points (filter not-empty (split obstacles #";")))
+     :busters (mapv parse-buster (filter not-empty (split busters #";")))}))
+
